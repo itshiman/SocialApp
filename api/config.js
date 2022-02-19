e7383ebfa1acd6c86b3011fec35f81adbba3c6b4
Himanshu <himanshu092017@gmail.com>
2022-02-19 14:00:00 +0530

    Seperated frontEnd from other modules

--- api/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  secretKey: '12345-67890-09876-54321',
-  mongoUrl:
-    'mongodb+srv://Admin:jWVYBWhMjgnkdOJ6@cluster0.kks82.mongodb.net/SocialServerDatabase?retryWrites=true&w=majority',
-};
